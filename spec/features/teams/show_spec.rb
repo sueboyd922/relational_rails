cf8cff666c916f66068d774b0928e12cd7dd168f
Susan Boyd <sueboyd922@gmail.com>
2022-03-22 22:24:21 -0600

    Add show view for teams

---
+++ spec/features/teams/show_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'teams show page' do
+  it 'shows all teams' do
+    team_1 = Team.create!(name: 'Dakstreet Boys', games_played: 9, winning_record: false)
+    team_2 = Team.create!(name: 'Can You Diggs It', games_played: 10, winning_record: true)
+
+    visit "/teams/#{team_1.id}"
+
+    save_and_open_page
+    expect(page).to have_content(team_1.name)
+    expect(page).to have_content(team_1.games_played)
+    expect(page).to have_content(team_1.winning_record)
+  end
+end
