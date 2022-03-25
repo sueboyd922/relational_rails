0c1e197c083c57f87f68069aa203611618ffa0d8
Susan Boyd <sueboyd922@gmail.com>
2022-03-24 22:17:20 -0400

    Test: write test for displaying number of players for a team on their show page

--- spec/features/teams/show_spec.rb
+++ spec/features/teams/show_spec.rb
@@ -15,4 +15,16 @@ RSpec.describe 'teams show page' do
 
     expect(page).not_to have_content(team_2.name)
   end
+
+  it 'shows number of players for each team' do
+    team_1 = Team.create!(name: 'Dakstreet Boys', games_played: 9, winning_record: false)
+    player_1 = Player.create!(name: 'Josh Allen', position: 'QB', points: 312, active: true, team_id: team_1.id)
+    player_2 = Player.create!(name: 'Derrick Henry', position: 'RB', points: 260, active: true, team_id: team_1.id)
+
+    visit "/teams/#{team_1.id}"
+
+    save_and_open_page
+
+    expect(page).to have_content(team_1.num_of_players)
+  end
 end
