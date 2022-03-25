c249a12104a2a8a147006d488ad7234006303f45
Susan Boyd <sueboyd922@gmail.com>
2022-03-24 20:34:12 -0400

    Test: write test for teams to show up in reverse order by created_at in index

--- spec/features/teams/index_spec.rb
+++ spec/features/teams/index_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe 'teams index page' do
     expect(page).to have_content(team_1.name)
     expect(page).to have_content(team_2.name)
   end
+
+  it 'shows all the teams in order of created_at, most recent first' do
+    team_1 = Team.create!(name: 'Dakstreet Boys', games_played: 9, winning_record: false)
+    team_2 = Team.create!(name: 'Can You Diggs It', games_played: 10, winning_record: true)
+    team_3 = Team.create!(name: 'Gym Tannehill Laundry', games_played: 9, winning_record: true)
+    team_4 = Team.create!(name: 'Red Solo Kupp', games_played: 10, winning_record: false)
+
+    visit '/teams'
+
+    save_and_open_page
+
+    expect(team_4.name).to appear_before(team_3.name)
+    expect(team_3.name).to appear_before(team_2.name)
+    expect(team_2.name).to appear_before(team_1.name)
+    exepect(team_4.created_at).to appear_before(team_3.created_at)
+    exepect(team_3.created_at).to appear_before(team_2.created_at)
+    exepect(team_2.created_at).to appear_before(team_1.created_at)
+  end
 end
--- spec/features/teams/team_players_spec.rb
+++ spec/features/teams/team_players_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe 'team players page', type: :feature do
 
     save_and_open_page
 
-    expect(page).to have_content(team_1.name)
     expect(page).not_to have_content(team_2.name)
     expect(page).to have_content(player_1.name)
     expect(page).to have_content(player_1.position)
@@ -24,4 +23,19 @@ RSpec.describe 'team players page', type: :feature do
     expect(page).to have_content(player_2.active)
     expect(page).not_to have_content(player_3.name)
   end
+
+  it 'will not show information on a different team' do
+    team_1 = Team.create!(name: 'Dakstreet Boys', games_played: 9, winning_record: false)
+    team_2 = Team.create!(name: 'Can You Diggs It', games_played: 10, winning_record: true)
+    player_1 = Player.create!(name: 'Josh Allen', position: 'QB', points: 312, active: true, team_id: team_1.id)
+    player_2 = Player.create!(name: 'Derrick Henry', position: 'RB', points: 260, active: true, team_id: team_1.id)
+    player_3 = Player.create!(name: 'Davante Adams', position: 'WR', points: 275, active: false, team_id: team_2.id)
+
+    visit "/teams/#{team_1.id}/players"
+
+    save_and_open_page
+    expect(page).to have_content(team_1.name)
+    expect(page).not_to have_content(player_3.name)
+  end
+
 end
