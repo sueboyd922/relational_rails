1baa56262c943a7b459c24b9871fa22cc65420a5
Susan Boyd <sueboyd922@gmail.com>
2022-03-28 13:59:43 -0400

    Refactor: change new team form to take in a drop down for winning_record

--- spec/features/teams/new_spec.rb
+++ spec/features/teams/new_spec.rb
@@ -2,16 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'teams index page' do
   it 'can create a new team' do
-    visit "/teams/new"
+    visit '/teams'
+
+    click_link("New Team")
+    expect(current_path).to eq("/teams/new")
 
     fill_in("Name", with: "No Punt Intended")
-    fill_in("Winning record", with: false)
+    # fill_in("Winning record", with: false)
+    select "false", :from => :winning_record
     fill_in("Games played", with: 10)
-    click_button("Create Team")
-    new_team = Team.last
+    click_on("Create Team")
+    # new_team = Team.last
     expect(current_path).to eq("/teams")
-    expect(page).to have_content(new_team.name)
+    # expect(page).to have_content(new_team.name)
+    expect(page).to have_content("No Punt Intended")
   end
-
-
 end
