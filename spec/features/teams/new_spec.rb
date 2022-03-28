1fba08279b22aa3074061a8d79c815610b61540c
Susan Boyd <sueboyd922@gmail.com>
2022-03-27 22:47:16 -0400

    Test: write test for form to create new team

--- spec/features/teams/new_spec.rb
+++ spec/features/teams/new_spec.rb
@@ -1,19 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'teams index page' do
-  before (:each) do
-    @team_1 = Team.create!(name: 'Dakstreet Boys', games_played: 9, winning_record: false)
-    @team_2 = Team.create!(name: 'Can You Diggs It', games_played: 10, winning_record: true)
-    @team_3 = Team.create!(name: 'Gym Tannehill Laundry', games_played: 9, winning_record: true)
-    @team_4 = Team.create!(name: 'Red Solo Kupp', games_played: 10, winning_record: false)
+  it 'can create a new team' do
+    visit "/teams/new"
+
+    fill_in("Name", with: "No Punt Intended")
+    fill_in("Winning record", with: false)
+    fill_in("Games played", with: 10)
+    click_button("Create Team")
+    new_team_id = Team.last
+    expect(current_path).to eq("/teams")
   end
 
-  # it 'links to create a new team' do
-  #   visit '/teams'
-  #
-  #   expect(page).to have_link("New Team")
-  #   save_and_open_page
-  #   click_link("New Team")
-  #   expect(current_path).to eq("/teams/new")
-  # end
+
 end
