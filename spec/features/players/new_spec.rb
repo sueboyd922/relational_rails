89c3dd6819950fbfec03e0e6b207afa4c87a967b
Susan Boyd <sueboyd922@gmail.com>
2022-03-28 21:31:21 -0400

    Test: Write test for creation of a new player

--- spec/features/players/new_spec.rb
+++ spec/features/players/new_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe 'new player page' do
     click_link("Add New Player")
     expect(current_path).to eq("/teams/#{@team.id}/players/new")
   end
+
+  it 'creates a new player for a specific team' do
+    visit "/teams/#{@team.id}/players/new"
+
+    fill_in("Name", with: "Gabe Davis")
+    fill_in("Position", with: "WR")
+    select("true", from: :active)
+    fill_in("Points", with: 118)
+    click_on("Draft Player")
+
+    expect(current_path).to eq("/teams/#{@team.id}/players")
+    expect(page).to have_content("Gabe Davis")
+    expect(page).to have_content("WR")
+    expect(page).to have_content(118)
+  end
 end
